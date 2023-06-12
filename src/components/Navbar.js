ce1196d10a06c09b8c6a6036b2fd7be8a8dd0fef
rae parikesit <raeparikesit@gmail.com>
2023-06-12 22:04:22 +0700

    fix eslint

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { styles } from '@/styles/styles'
 import { BiLogIn } from 'react-icons/bi'
 
@@ -7,13 +8,13 @@ const Navbar = () => {
       <div
         className={`${styles.mainMaxWidth} ${styles.mainRow} !justify-between px-8`}
       >
-        <div className="w-full flex-col justify-start p-5">
-          <p className="text-2xl font-bold text-main-purple">FlyWise</p>
-          <p className="text-lg text-main-purple">Your Traveling Parner</p>
+        <div className='w-full flex-col justify-start p-5'>
+          <p className='text-2xl font-bold text-main-purple'>FlyWise</p>
+          <p className='text-lg text-main-purple'>Your Traveling Parner</p>
         </div>
 
-        <div className="w-full flex justify-end items-center">
-          <button className="flex gap-5 items-center justify-center bg-main-orange p-4 rounded-xl text-white hover:scale-110 duration-300">
+        <div className='w-full flex justify-end items-center'>
+          <button className='flex gap-5 items-center justify-center bg-main-orange p-4 rounded-xl text-white hover:scale-110 duration-300'>
             <BiLogIn />
             <p>Masuk</p>
           </button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import MainHome from '@/views/home/MainHome'
 
 const index = () => {
