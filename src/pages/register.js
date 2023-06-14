b340a0b519047c9606281a06715821527d5a1a16
arviollaarf <arviollaff@gmail.com>
2023-06-14 10:33:54 +0700

    ubah form regis

--- src/pages/register.js
+++ src/pages/register.js
@@ -20,24 +20,24 @@ const Register = () => {
 
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-1 text-left text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="mt-10 text-left text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Daftar
           </h2>
         </div>
-        <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label
                 htmlFor="nama"
-                className="block text-sm font-medium text-gray-900"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Nama
               </label>
-              <div className="mt-1">
+              <div className="mt-2">
                 <input
                   id="nama"
                   name="nama"
-                  type="nama"
+                  type="text"
                   autoComplete="nama"
                   required
                   className="block w-full rounded-2xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -49,59 +49,51 @@ const Register = () => {
               <div className="flex items-center justify-between">
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium text-gray-900"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Email
                 </label>
               </div>
-              <div className="mt-1">
+              <div className="mt-2">
                 <input
                   id="email"
                   name="email"
-                  type="email"
+                  type="text"
                   autoComplete="email"
                   required
                   className="block w-full rounded-2xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-
             <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="notelp"
-                  className="block text-sm font-medium  text-gray-900"
-                >
-                  Nomor Telepon
-                </label>
-              </div>
-              <div className="mt-1">
+              <label
+                htmlFor="notelp"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Nomor Telepon
+              </label>
+              <div className="mt-2">
                 <input
                   id="notelp"
                   name="notelp"
-                  type="notelp"
-                  autoComplete="notelp"
+                  type="text"
                   required
                   className="block w-full rounded-2xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-
             <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="buatpass"
-                  className="block text-sm font-medium  text-gray-900"
-                >
-                  Buat Password
-                </label>
-              </div>
-              <div className="mt-1">
+              <label
+                htmlFor="buatpass"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Buat Password
+              </label>
+              <div className="mt-2">
                 <input
                   id="buatpass"
                   name="buatpass"
-                  type="buatpass"
-                  autoComplete="buatpass"
+                  type="text"
                   required
                   className="block w-full rounded-2xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -117,7 +109,6 @@ const Register = () => {
               </button>
             </div>
           </form>
-
           <p className="mt-10 text-center text-sm text-gray-500">
             Sudah punya akun?{" "}
             <a
