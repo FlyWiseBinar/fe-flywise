bebdd02393f259b662b3028ce082780fd7cb5d32
ganden16 <alvindian85@gmail.com>
2023-06-20 20:48:01 +0700

    trigger datepicker

--- src/components/orderHistory/Button.js
+++ src/components/orderHistory/Button.js
@@ -1,7 +1,7 @@
-import { styles } from "@/styles/styles"
-import { LuArrowLeft, LuSearch, LuFilter, LuX } from "react-icons/lu"
-import { AiOutlineSearch } from "react-icons/ai"
-import { useState, useRef } from "react"
+import {styles} from "@/styles/styles"
+import {LuArrowLeft, LuSearch, LuFilter, LuX} from "react-icons/lu"
+import {AiOutlineSearch} from "react-icons/ai"
+import {useState} from "react"
 import Datepicker from "react-tailwindcss-datepicker"
 import Head from "next/head"
 
@@ -10,94 +10,87 @@ import Link from "next/link"
 
 
 const Button = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const reference = useRef(null)
+	const [isOpenModal, setIsOpenModal] = useState(false)
 
-  const openDatePicker = () => {
-    reference.current()
-  }
-  const openModal = () => {
-    setIsOpen(true)
-  }
+	const openDatePicker = () => {
+		document.getElementById('datepicker').focus()
+	}
+	
+	const handleClickModal = () => {
+		setIsOpenModal(!isOpenModal)
+	}
 
-  const closeModal = () => {
-    setIsOpen(false)
-  }
+	const [value, setValue] = useState({
+		startDate: new Date(),
+		endDate: new Date().setMonth(11),
+	})
 
-  const [value, setValue] = useState({
-    startDate: new Date(),
-    endDate: new Date().setMonth(11),
-  })
-
-  const handleValueChange = (newValue) => {
-    console.log("newValue:", newValue)
-    setValue(newValue)
-  }
-
-
-  return (
-    <>
-      <div className={`${styles.mainRow} gap-3`}>
-        <div>
-          <Link href="/" className="flex gap-5 md:justify-end items-center bg-third-purple p-3 md:ml-auto rounded-xl text-white hover:bg-main-purple md:pr-100 duration-75">
-            {" "}
-            <LuArrowLeft /> Beranda
-          </Link>
-        </div>
-        <div>
-          <button className="flex gap-2 md:justify-end items-center border border-main-purple py-1 rounded-full duration-75 md:px-5" onClick={openDatePicker}>
-            {" "}
-            <LuFilter />
-            Filter
-          </button>
-          <div className="hidden">  <Datepicker 
-          ref = {reference}
-          primaryColor={"violet"}
-          showFooter={true}
-          useRange={false}
-          value={value}
-          placeholder={"halo"}
-          onChange={handleValueChange}
-          configs={{
-            footer: {
-            cancel: "Batal", 
-            apply: "Simpan" 
-            }
-            }} 
-        /></div>
-        </div>
-        <div
-          className="text-main-purple hover:text-third-purple text-4xl"
-          onClick={openModal}
-        >
-          <AiOutlineSearch />
-        </div>
-        {isOpen && (
-          <div className="fixed inset-0 bg-opacity-70 bg-black flex items-center justify-center">
-            <div className="absolute bg-white rounded-lg shadow-2xl w-96 h-72">
-              <div className="flex gap-3 items-center p-4 ">
-                <input
-                  type="text"
-                  placeholder="  Masukkan Nomor Penerbangan"
-                  className="py-1 px-3 border-2 w-full rounded-lg border-gray-300"
-                />{" "}
-                <div
-                  className="text-2xl hover:text-gray-400"
-                  onClick={closeModal}
-                >
-                  <LuX />
-                </div>
-              </div>
-              <hr className="bg-black" />
-            </div>
-          </div>
-        )}
-      </div>
-      <div className="flex justify-center pt-3">
-        <div className="w-7/12 border-t-2 border-slate-200"></div>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className={`${styles.mainRow} gap-3`}>
+				<div>
+					<Link href="/" className="flex gap-5 md:justify-end items-center bg-third-purple p-3 md:ml-auto rounded-xl text-white hover:bg-main-purple md:pr-100 duration-75">
+						{" "}
+						<LuArrowLeft /> Beranda
+					</Link>
+				</div>
+				<div>
+					<button className="flex gap-2 md:justify-end items-center border border-main-purple py-1 rounded-full duration-75 md:px-5 rela" onClick={openDatePicker}>
+						{" "}
+						<LuFilter />
+						Filter
+					</button>
+					<Datepicker
+						inputId="datepicker"
+						inputClassName={'bg-white focus:outline-none opacity-0 pointer-events-none'}
+						primaryColor={"violet"}
+						showFooter={true}
+						useRange={false}
+						value={value}
+						placeholder={" "}
+						toggleClassName={"opacity-0 pointer-events-none	"}
+						onChange={(e)=>setValue(e.target.value)}
+						configs={{
+							footer: {
+								cancel: "Batal",
+								apply: "Simpan"
+							}
+						}}
+						popoverDirection="down"
+					/>
+				</div>
+				<div
+					className="text-main-purple hover:text-third-purple text-4xl cursor-pointer"
+					onClick={handleClickModal}
+				>
+					<AiOutlineSearch />
+				</div>
+				{isOpenModal && (
+					<div className="fixed inset-0 bg-opacity-70 bg-black flex items-center justify-center">
+						<div className="absolute bg-white rounded-lg shadow-2xl w-96 h-72">
+							<div className="flex gap-3 items-center p-4 ">
+								<input
+									type="text"
+									placeholder="  Masukkan Nomor Penerbangan"
+									className="py-1 px-3 border-2 w-full rounded-lg border-gray-300"
+								/>{" "}
+								<div
+									className="text-2xl hover:text-gray-400 cursor-pointer"
+									onClick={handleClickModal}
+								>
+									<LuX />
+								</div>
+							</div>
+							<hr className="bg-black" />
+						</div>
+					</div>
+				)}
+			</div>
+			<div className="flex justify-center pt-3">
+				<div className="w-7/12 border-t-2 border-slate-200"></div>
+			</div>
+		</>
+	)
 }
 
 export default Button
