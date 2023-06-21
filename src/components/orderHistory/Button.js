4150b596d75ccd42c856e395e4a8b21c3408ede0
yogangr <ynugroho487@gmail.com>
2023-06-21 22:52:23 +0700

    update:search history

--- src/components/orderHistory/Button.js
+++ src/components/orderHistory/Button.js
@@ -5,101 +5,118 @@ import { useState } from "react"
 import Datepicker from "react-tailwindcss-datepicker"
 import Head from "next/head"
 
+
 import React from "react"
 import Link from "next/link"
 
-const Button = () => {
-  const [isOpenModal, setIsOpenModal] = useState(false)
 
-  const openDatePicker = () => {
-    document.getElementById("datepicker").focus()
-  }
+const Button = ({ onSearch }) => {
+	const [isOpenModal, setIsOpenModal] = useState(false)
 
-  const handleClickModal = () => {
-    setIsOpenModal(!isOpenModal)
-  }
+	const [searchQuery, setSearchQuery] = useState("")
 
-  const [value, setValue] = useState({
-    startDate: new Date(),
-    endDate: new Date().setMonth(11),
-  })
+	const handleInputChange = (e) => {
+		setSearchQuery(e.target.value)
+	}
 
-  return (
-    <>
-      <div
-        className="flex flex-row justify-center items-center md:w-full md:gap-3 gap-2"
-      >
-        <div className="">
-          <Link
-            href="/"
-            className="flex gap-5 md:justify-end items-center bg-third-purple p-3 md:ml-auto rounded-xl text-white hover:bg-main-purple lg:pr-100 md:pr-96 pr-28 duration-75"
-          >
-            {" "}
-            <LuArrowLeft /> Beranda
-          </Link>
-        </div>
-        <div>
-          <button
-            className="flex gap-2 md:justify-end items-center border border-main-purple py-1 rounded-full duration-75 md:px-5 px-3"
-            onClick={openDatePicker}
-          >
-            {" "}
-            <LuFilter />
-            Filter
-          </button>
-          <Datepicker
-            inputId="datepicker"
-            inputClassName={
-              "absolute flex text-xs bg-white focus:outline-none opacity-0 pointer-events-none"
-            }
-            primaryColor={"violet"}
-            showFooter={true}
-            useRange={false}
-            value={value}
-            placeholder={" "}
-            toggleClassName={"absolute opacity-0 pointer-events-none "}
-            onChange={(e) => setValue(e.target.value)}
-            configs={{
-              footer: {
-                cancel: "Batal",
-                apply: "Simpan",
-              },
-            }}
-            popoverDirection="down"
-          />
-        </div>
-        <div
-          className="text-main-purple hover:text-third-purple md:text-4xl text-2xl cursor-pointer"
-          onClick={handleClickModal}
-        >
-          <AiOutlineSearch />
-        </div>
-        {isOpenModal && (
-          <div className="fixed inset-0 bg-opacity-70 bg-black flex items-center justify-center">
-            <div className="absolute bg-white rounded-lg shadow-2xl md:w-96 w-80 md:h-72 h-64">
-              <div className="flex gap-3 items-center p-4 cursor-pointer">
-                <input
-                  type="text"
-                  placeholder="  Masukkan Nomor Penerbangan"
-                  className="py-1 px-3 border-2 w-full rounded-lg md:text-base text-sm border-gray-300"
-                />{" "}
-                <div
-                  className="text-2xl hover:text-gray-400 cursor-pointer"
-                  onClick={handleClickModal}
-                >
-                  <LuX />
-                </div>
-              </div>
-              <hr className="bg-black" />
-            </div>
-          </div>
-        )}
-      </div>
-      <div className="flex justify-center mt-3">
-        <div className="lg:w-7/12 w-11/12 border-t-2 border-slate-300"></div>
-      </div>
-    </>
-  )
+	const handleFormSubmit = (e) => {
+		e.preventDefault()
+		onSearch(searchQuery)
+	}
+
+	const openDatePicker = () => {
+		document.getElementById("datepicker").focus()
+	}
+
+	const handleClickModal = () => {
+		setIsOpenModal(!isOpenModal)
+	}
+
+	const [value, setValue] = useState({
+		startDate: new Date(),
+		endDate: new Date().setMonth(11),
+	})
+
+	return (
+		<>
+			<div
+				className="flex flex-row justify-center items-center md:w-full md:gap-3 gap-2"
+			>
+				<div className="">
+					<Link
+						href="/"
+						className="flex gap-5 md:justify-end items-center bg-third-purple p-3 md:ml-auto rounded-xl text-white hover:bg-main-purple lg:pr-100 md:pr-96 pr-28 duration-75"
+					>
+						{" "}
+						<LuArrowLeft /> Beranda
+					</Link>
+				</div>
+				<div>
+					<button
+						className="flex gap-2 md:justify-end items-center border border-main-purple py-1 rounded-full duration-75 md:px-5 px-3"
+						onClick={openDatePicker}
+					>
+						{" "}
+						<LuFilter />
+						Filter
+					</button>
+					<Datepicker
+						inputId="datepicker"
+						inputClassName={
+							"absolute flex text-xs bg-white focus:outline-none opacity-0 pointer-events-none"
+						}
+						primaryColor={"violet"}
+						showFooter={true}
+						useRange={false}
+						value={value}
+						placeholder={" "}
+						toggleClassName={"absolute opacity-0 pointer-events-none "}
+						onChange={(e) => setValue(e.target.value)}
+						configs={{
+							footer: {
+								cancel: "Batal",
+								apply: "Simpan",
+							},
+						}}
+						popoverDirection="down"
+					/>
+				</div>
+				<div
+					className="text-main-purple hover:text-third-purple md:text-4xl text-2xl cursor-pointer"
+					onClick={handleClickModal}
+				>
+					<AiOutlineSearch />
+				</div>
+				{isOpenModal && (
+					<div className="fixed inset-0 bg-opacity-70 bg-black flex items-center justify-center">
+						<div className="absolute bg-white rounded-lg shadow-2xl md:w-96 w-80 md:h-72 h-64">
+							<div className="flex gap-3 items-center p-4 cursor-pointer">
+								<form onSubmit={handleFormSubmit} className="w-full">
+									<input
+										type="text"
+										placeholder="  Masukkan Nomor Penerbangan"
+										className="py-1 px-3 border-2 w-full rounded-lg md:text-base text-sm border-gray-300"
+										value={searchQuery}
+										onChange={handleInputChange}
+									/>{" "}
+								</form>
+								<div
+									className="text-2xl hover:text-gray-400 cursor-pointer"
+									onClick={handleClickModal}
+								>
+									<LuX />
+								</div>
+							</div>
+							<hr className="bg-black" />
+						</div>
+					</div>
+				)}
+			</div>
+			<div className="flex justify-center mt-3">
+				<div className="lg:w-7/12 w-11/12 border-t-2 border-slate-300"></div>
+			</div>
+		</>
+	)
 }
 
 export default Button
--- src/views/orderHistory/MainOrderHistory.js
+++ src/views/orderHistory/MainOrderHistory.js
@@ -1,13 +1,62 @@
 import React from "react"
 import Button from "@/components/orderHistory/Button"
+import AccordionHistory from "@/components/orderHistory/AccordionHistory"
+import { useState, useEffect } from "react"
 
 const MainOrderHistory = () => {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://be-flywise-stagging-jcbxz3zpbq-as.a.run.app/v1/api/order/history"
+      )
+      const { orders } = await response.json()
+      const jsonData = orders
+      setData(jsonData)
+      setLoading(false)
+    } catch (error) {
+      console.log("Error fetching data:", error)
+    }
+  }
+
+  const fetchFilteredOrders = async (searchQuery) => {
+    try {
+      const response = await fetch(`https://be-flywise-stagging-jcbxz3zpbq-as.a.run.app/v1/api/order/historysearch?orderCode=${searchQuery}`)
+      const { orders } = await response.json()
+      const data = orders
+      setData(data)
+    } catch (error) {
+      console.error("Error fetching filtered order history:", error)
+    }
+  }
+
+  const handleSearch = async (searchQuery) => {
+    if (searchQuery.trim() !== "") {
+      await fetchFilteredOrders(searchQuery)
+    } else {
+      setLoading(true)
+      fetchData()
+    }
+  }
+
   return (
     <div className="md:mb-16">
       <div className="md:w-1/2 md:ml-0 ml-3 flex md:justify-center">
         <div className={"py-10 font-bold text-xl"}>Riwayat Pemesanan</div>
       </div>
-      <Button/>
+      <Button
+        onSearch={handleSearch}
+      />
+      <AccordionHistory
+        orders={data}
+        loading={loading}
+      />
     </div>
   )
 }
