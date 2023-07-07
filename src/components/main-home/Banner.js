6d8d8a22ea244a8a2694a210f00c8e447339d29e
ANgafifudin <adjingafifudin@gmail.com>
2023-07-07 15:29:53 +0700

    fix: discount

--- src/components/main-home/Banner.js
+++ src/components/main-home/Banner.js
@@ -1,8 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { styles } from "@/styles/styles"
 
 const Banner = () => {
-  const randomNumber = Math.floor(Math.random() * 100)
+  const [randomNumber, setRandomNumber] = useState(0)
+
+  useEffect(() => {
+    const updateRandomNumber = () => {
+      const newRandomNumber = Math.floor(Math.random() * 100)
+      setRandomNumber(newRandomNumber)
+    }
+
+    // Update angka acak setiap 6 jam (21600000 ms)
+    updateRandomNumber() // Pertama kali saat komponen di-mount
+    const interval = setInterval(updateRandomNumber, 3600000)
+
+    return () => {
+      clearInterval(interval) // Membersihkan interval saat komponen di-unmount
+    }
+  }, [])
+
   return (
     <div className={`${styles.mainCol} py-5`}>
       <div className="w-full bg-main-orange h-[150px] absolute z-0 top-52"></div>
